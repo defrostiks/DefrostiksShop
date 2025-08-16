b90f89130a39f33ef1c686dd2d2992bbc0e50947
defrostiks <vvvalery4life@gmail.com>
2025-08-16 21:07:15 +0300

    added the possibility of registration and entry into the database

--- app/src/main/java/com/example/defrostiksshop/MainActivity.kt
+++ app/src/main/java/com/example/defrostiksshop/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.defrostiksshop
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +15,30 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val userLogin = findViewById<EditText>(R.id.userLogin)
+        val userEmail = findViewById<EditText>(R.id.userEmail)
+        val userPass = findViewById<EditText>(R.id.userPass)
+        val buttonReg = findViewById<Button>(R.id.buttonReg)
+
+        buttonReg.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val pass = userPass.text.toString().trim()
+
+            if (login == "" || email == "" || pass == ""){
+                Toast.makeText(this, "Заполните необходимые поля", Toast.LENGTH_LONG).show()
+            } else {
+                val user = User(login, email, pass)
+
+                val db = DbHelper(this, null)
+                db.addUser(user)
+                Toast.makeText(this, "Пользователь $login добавлен", Toast.LENGTH_LONG).show()
+
+                userLogin.text.clear()
+                userEmail.text.clear()
+                userPass.text.clear()
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
---
+++ app/src/main/java/com/example/defrostiksshop/User.kt
@@ -0,0 +1,4 @@
+package com.example.defrostiksshop
+
+class User(val login: String, val email: String, val pass: String) {
+}
